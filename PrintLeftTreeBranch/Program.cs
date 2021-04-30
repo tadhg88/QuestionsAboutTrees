d5e0eb504edf5a0bf540201e305f7d1257c6bfa2
tadhg88 <tadhg88@gmail.com>
2021-04-30 11:04:29 +0100

    add method to print left hand saide view of tree using Queue

--- PrintLeftTreeBranch/Program.cs
+++ PrintLeftTreeBranch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PrintLeftTreeBranch
 {
@@ -30,6 +31,9 @@ namespace PrintLeftTreeBranch
             findNodes.FindFirstVisibleNodes(root, 1);
             Console.WriteLine();
 
+            findNodes.PrintLeftViewNodes_UsingQueue(root);
+            Console.WriteLine();
+
             findNodes.previousMaxLevel = 0;
             findNodes.FindLeftTreeNodes(root, 1);
 
@@ -42,6 +46,12 @@ namespace PrintLeftTreeBranch
     {
         public int previousMaxLevel = 0;
 
+        /// <summary>
+        /// Given a Binary Tree, print left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from left side.
+        /// https://www.geeksforgeeks.org/print-left-view-binary-tree/
+        /// </summary>
+        /// <param name="root">Current node being traversed.</param>
+        /// <param name="currentLevel">Level of current node.</param>
         public void FindFirstVisibleNodes(Node root, int currentLevel)
         {
             if (root == null)
@@ -56,6 +66,43 @@ namespace PrintLeftTreeBranch
 
             FindFirstVisibleNodes(root.Left, currentLevel+1);
             FindFirstVisibleNodes(root.Right, currentLevel+1);
+        }
+
+        /// <summary>
+        /// Print left view of binary tree using queue
+        /// </summary>
+        /// <param name="root"></param>
+        public void PrintLeftViewNodes_UsingQueue(Node root)
+        {
+            if (root == null)
+                return;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                // number of nodes at current level
+                int n = queue.Count;
+
+                // Traverse all nodes of current level
+                for (int i = 1; i <= n; i++)
+                {
+                    Node temp = queue.Dequeue();
+
+                    // Print the left most element at the level
+                    if (i == 1)
+                        Console.Write(temp.Data + " ");
+
+                    // Add left node to queue
+                    if (temp.Left != null)
+                        queue.Enqueue(temp.Left);
+
+                    // Add right node to queue
+                    if (temp.Right != null)
+                        queue.Enqueue(temp.Right);
+                }
+            }
         }
 
         public void FindLeftTreeNodes(Node root, int currentLevel)
